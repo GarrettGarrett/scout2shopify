aa91b3cda5c78776e862d5b267ea8eea18bc43a0
PyPy <skfelix1@gmail.com>
2022-01-01 11:15:33 -0800

    update footer year

--- components/Footer.js
+++ components/Footer.js
@@ -69,7 +69,7 @@ const navigation = [
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
          
           <div className="mt-8 md:mt-0 md:order-1">
-            <p className="text-center text-base text-gray-400">&copy; 2021 All rights reserved. Built by Garrett F. with Next.js</p>
+            <p className="text-center text-base text-gray-400">&copy; 2022 All rights reserved. Built by Garrett F. with Next.js</p>
           </div>
         </div>
       </footer>
